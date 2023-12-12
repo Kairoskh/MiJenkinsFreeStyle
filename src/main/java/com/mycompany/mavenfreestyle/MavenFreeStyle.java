a642e7124b0191a185f421b92c9ca6198f9cf16c
Kairoskh <93041953+Kairoskh@users.noreply.github.com>
2023-12-12 18:36:46 +0100

    Update MavenFreeStyle.java

--- src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -17,8 +17,8 @@ public class MavenFreeStyle {
     
     public int fibonacci(int n){	
 	if (n <= 1)
-        return n;
-    else
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        	return n;
+    	else
+        	return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
